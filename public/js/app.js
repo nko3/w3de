ab19f46846c5f84a4ea6b8e284cc7376c58655c1
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-11-11 21:38:31 +0530

    Desktop interface finished

--- public/js/app.js
+++ public/js/app.js
@@ -51,7 +51,9 @@
   }
 
   var initiateLoginManager = function () {
-    if (!window.Session || !window.Session.logged) {
+    if (window.Session && window.Session.logged) {
+      initiateDesktop();
+    } else {
       key('enter', 'login', function () {
         var inputs = $('#login input')
           , username = inputs[0].value
@@ -60,6 +62,12 @@
         $('#login form').hide();
         $('#login .loader').show();
 
+        var loginFailed = function () {
+          $('#login .loader').hide();
+          $('#login form').show();
+          return ui.error('Error', 'Login failed').effect('slide');
+        }
+
         $.ajax({
           type: "POST",
           url: "/login",
@@ -67,11 +75,7 @@
           dataType: 'json',
           success: function (data) {
             if (!data.success) {
-              $('#login .loader').hide();
-              $('#login form').show();
-
-              ui.error('Error', 'Login failed').effect('slide');
-              return;
+              return loginFailed();
             }
 
             window.Session = data.session;
@@ -79,6 +83,11 @@
             toggleKeymasterFilter();
             initiateDesktop();
             $('#login').trigger('reveal:close');
+
+            ui.success('Success', 'Logged in').effect('slide');
+          },
+          error: function () {
+            return loginFailed();
           }
         });
       });
@@ -92,9 +101,71 @@
       });
 
       $('body').removeClass('loader');
-    } else {
-      initiateDesktop();
     }
+  };
+
+  var setDesktopKeymaster = function () {
+    var r = 1, c = 1, rMax = 1, cMax = 2;
+
+    var removeActive = function () {
+      $('#r' + r + ' #c' + c).removeClass('active');
+    };
+
+    var addActive = function () {
+      while (r <= 0) r+=rMax;
+      while (r > rMax) r-=rMax;
+      while (c <= 0) c+=cMax;
+      while (c > cMax) c-=cMax;
+      $('#r' + r + ' #c' + c).addClass('active');
+    };
+
+    key('enter', 'desktop', function () {
+      $('#r' + r + ' #c' + c).click();
+      return false;
+    });
+
+    key('down', 'desktop', function () {
+      removeActive();
+      r += 1;
+      addActive();
+      return false;
+    });
+
+    key('up', 'desktop', function () {
+      removeActive();
+      r -= 1;
+      addActive();
+      return false;
+    });
+
+    key('right', 'desktop', function () {
+      removeActive();
+      c += 1;
+      addActive();
+      return false;
+    });
+
+    key('left', 'desktop', function () {
+      removeActive();
+      c -= 1;
+      addActive();
+      return false;
+    });
+
+    key('h', 'desktop', function () {
+      $('#help-desktop').reveal();
+      return false;
+    })
+
+    key.setScope('desktop');
+  };
+
+  var initiateDesktop = function () {
+    setDesktopKeymaster();
+    $('body').addClass('desktop');
+    $('#desktop').show();
+
+    ui.notify('Help', 'Press H for help').effect('slide').sticky();
   };
 
   var initiateMenu = function () {
